8abf124b5a3c92731cb91768a5008c01817df300
Cody Brubaker and Ezra Sandzer-Bell <cody@>
2016-10-19 09:27:52 -0700

    create a form that retrieves a word and capitalizes it using javascript and jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,8 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var nameInput = $("input#name").val();
-    var verbPresentInput = $("input#verbPresent").val();
-    var person1Input = $("input#person1").val();
-    var nounInput= $("input#noun").val();
-    var verbPastInput = $("input#verbPast").val();
-    var person2Input = $("input#person2").val();
-    var verbPresentTwoInput = $("input#verbPresentTwo").val();
-    var animalInput = $("input#animal").val();
+    var wordInput = $("input#word").val().toUpperCase();
 
-    $(".name").text(nameInput);
-    $(".verbPresent").text(verbPresentInput);
-    $(".person1").text(person1Input);
-    $(".noun").text(nounInput);
-    $(".verbPast").text(verbPastInput);
-    $(".person2").text(person2Input);
-    $(".verbPresentTwo").text(verbPresentTwoInput);
-    $(".animal").text(animalInput);
+    $(".word").text(wordInput);
 
     $("#story").show();
 
